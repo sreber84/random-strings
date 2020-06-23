a02d6cebd10c1b418206384f4e164f7c946b6b3e
Simon Reber <sreber@redhat.com>
2020-06-23 17:01:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is just a simple string generator, showing date and then a random string to test various scenarios.
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func RandomString(n int) string {
+
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+func main() {
+
+	for {
+		currentTime := time.Now()
+		fmt.Println(currentTime.Format("2006-01-02 15:04:05.000000000"), " - ", RandomString(1000))
+	}
+}
